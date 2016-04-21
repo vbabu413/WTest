d42b033c89d4d9615b5447db869fd6b495be808d
vbabu413 <dvbabu6@gmail.com>
2016-04-21 13:56:17 +0530

    Chane driver get url

--- src/test/java/example/SecondTest.java
+++ src/test/java/example/SecondTest.java
@@ -1,8 +1,6 @@
 package example;
 
 import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
-import org.testng.annotations.Test;
 import org.openqa.selenium.By;      
 import org.openqa.selenium.WebDriver;       
 import org.openqa.selenium.firefox.FirefoxDriver;       
@@ -11,18 +9,22 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;   
 import org.testng.annotations.AfterTest; 
 public class SecondTest {
-	 private static WebDriver driver;       
-           
-     public static void main(String[] args) {   
-    	  driver = new FirefoxDriver();  
-         driver.get("http://www.guru99.com/selenium-tutorial.html");  
-         String title = driver.getTitle();                
-         AssertJUnit.assertTrue(title.contains("Free Selenium Tutorials"));       
-    
-       
-   
-   
-    
-         driver.quit();          
-     }  
+	private WebDriver driver;       
+    @Test              
+    public void testEasy() {    
+    //    driver.get("http://www.guru99.com/selenium-tutorial.html");  
+    	driver.get("http://www.google.com");
+        String title = driver.getTitle(); 
+        System.out.println(title);
+     //   Assert.assertTrue(title.contains("Free Selenium Tutorials"));       
+        
+    }   
+    @BeforeTest
+    public void beforeTest() {  
+        driver = new FirefoxDriver();  
+    }       
+    @AfterTest
+    public void afterTest() {
+        driver.quit();          
+    }      
 }
